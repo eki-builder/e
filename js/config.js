d477e05fb06597e794cc2b74c4ebac3ba34be24b
eki-builder <eki.builder@gmail.com>
2026-07-18 13:35:17 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -9,9 +9,9 @@ FILE OVERVIEW
 EXTERNAL SERVICE SETUP
 ========================= */
 const APPWRITE_ENDPOINT  = "https://nyc.cloud.appwrite.io/v1"; // e.g. https://nyc.cloud.appwrite.io/v1
-const APPWRITE_PROJECT   = "YOUR_PROJECT_ID";
+const APPWRITE_PROJECT   = "6a5990d90037af3e257f";
 
-const DB_ID       = "6a5990d90037af3e257f";   // Appwrite Database ID
+const DB_ID       = "6a599a40002e39a79325";   // Appwrite Database ID
 const COL_PROFILES      = "profiles";          // one doc per user: display name, theme pref
 const COL_PROJECTS      = "projects";          // one doc per generated website (content/config only)
 const COL_CONTENT       = "website_content";   // one doc per project: all form content (JSON)
